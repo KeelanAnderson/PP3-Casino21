b4c5098544b4ee5e3b770e888116656bce083170
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-11-22 18:40:03 +0000

    add game intro function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import random
+
+player_pot = 1000
+
+def game_intro():
+    print('Welcome To BlackJack!!!')
+    print(f"Your Starting Pot is ${player_pot}")
+    input('Please Enter Your Name: ')
+
+game_intro()
