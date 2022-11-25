6feb33d2b6d682bf9e714a345648c1911e91b7ec
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-11-25 23:27:21 +0000

    add card class

--- run.py
+++ run.py
@@ -4,84 +4,138 @@ import itertools
 
 values = {'Ace': 11, 'Two': 2, 'Three': 3, 'Four': 4,
           'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 
-          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 
-          'King': 10}
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
+suits = ('Diamonds', 'Hearts', 'Clubs', 'Spades')
+ranks = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
 
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-
-deck = list(itertools.product(values, suits))
-
-
+# deck = list(itertools.product(values, suits))
+starting_pot = 1000
 
 
-# starting_pot = 1000
+class Card:
+    """ creates instance of cards in deck """                                                                               
 
-# def game_intro():
-#     """ Gives introduction to the game. Shows players starting pot.
-#     prompts user to enter their name. """
+    def __init__(self, rank, suit):
+        self.rank
+        self.suit
 
-#     print('Welcome To BlackJack!!!')
-#     print(f"Your Starting Pot is ${starting_pot}")
-#     input('Please Enter Your Name: ')
-
-# game_intro()
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
 
 
-# def place_bet():
-#     """ Prompts user to input their bet amounts """
-#     print('Minimum bets are $50')
-#     while True:
-#         bet = int(input('Place your Bet: $'))
-#         if accept_bet(bet, starting_pot):
-#             remaining_pot = starting_pot - bet 
-#             print('Bet placed!')
-#             print(f"Pot: ${remaining_pot}")
-#             start_round(deck)
-#             break
+class Deck:
 
 
-# def accept_bet(bet_amount, pot):
-#     """ verifies if the bet amount is valid """
-#     try:
-#         if bet_amount > pot or bet_amount < 50:
-#             raise ValueError(
-#                 f"\nYou tried to bet ${bet_amount}\nYour Pot is ${pot}\nMinimum bets are $50\n"
-#             )
-#     except ValueError as error:
-#         print(error)
-#         print('Please try again...')
-#         return False
+    def __init__(deck):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
 
-#     return True
-
-# def start_round(deck):
-#     print('Dealer Shuffling Deck...')
-#     random.shuffle(deck)
-#     time.sleep(3)
-#     deal_card()
+    def __str__(self)
 
 
-def deal_card():
-    """ deals the next card from shuffled deck"""
+def game_intro():
+    """ Gives introduction to the game. Shows players starting pot.
+    prompts user to enter their name. """
+
+    print('Welcome To BlackJack!!!')
+    print(f"Your Starting Pot is ${starting_pot}")
+    input('Please Enter Your Name: ')
+
+game_intro()
+
+
+def place_bet():
+    """ Prompts user to input their bet amounts """
+    print('Minimum bets are $50')
+    while True:
+        bet = int(input('Place your Bet: $'))
+        if accept_bet(bet, starting_pot):
+            remaining_pot = starting_pot - bet 
+            print('Bet placed!')
+            print(f"Pot: ${remaining_pot}")
+            start_round(deck)
+            break
+
+
+def accept_bet(bet_amount, pot):
+    """ verifies if the bet amount is valid """
+    try:
+        if bet_amount > pot or bet_amount < 50:
+            raise ValueError(
+                f"\nYou tried to bet ${bet_amount}\nYour Pot is ${pot}\nMinimum bets are $50\n"
+            )
+    except ValueError as error:
+        print(error)
+        print('Please try again...')
+        return False
+
+    return True
+
+def start_round(deck):
+    print('Dealer Shuffling Deck...\n')
     random.shuffle(deck)
-    card_pulled = deck.pop()
-    card_value = values[card_pulled[0]]
-    card = card_pulled[0] + ' of ' + card_pulled[1]
-    print(card)
-    del card_pulled
-    return deck and card_value
+    time.sleep(3)
+    deal_first_hand()
 
 
-deal_card()
 
+
+
+place_bet()
 # player_score = []
+# dealer_score = []
 
 
-# def calculate_player_score(value):
+# def pull_card():
+#     """ deals the next card from shuffled deck"""
+#     random.shuffle(deck)
+#     card_pulled = deck.pop()
+#     card_value = values[card_pulled[0]]
+    
+#     card = card_pulled[0] + ' of ' + card_pulled[1]
+#     print(f"    {card}")
+#     del card_pulled
+#     return deck and card_value and card_pulled
+
+
+# def deal_dealer_card():
+#     """ deals the next card from shuffled deck"""
+#     random.shuffle(deck)
+#     card_pulled = deck.pop()
+#     card_value = values[card_pulled[0]]
+#     dealer_score.append(card_value)
+#     card = card_pulled[0] + ' of ' + card_pulled[1]
+#     print(f"    {card}")
+#     del card_pulled
+#     return deck and card_value
+
+
+# def deal_player_card():
+#     """ deals the next card from shuffled deck"""
+#     random.shuffle(deck)
+#     card_pulled = deck.pop()
+#     card_value = values[card_pulled[0]]
+#     player_score.append(card_value)
+#     card = card_pulled[0] + ' of ' + card_pulled[1]
+#     print(f"    {card}")
+#     del card_pulled
+#     return deck and card_value
+    
+
+# def calculate_score(value):
 #     """ calculate player score """
-#     player_score.append(value)
-#     Sum = sum(player_score)
+#     Sum = sum(value)
 #     print(Sum)
 
 
-# place_bet()
+
+
+
+# class Dealer:
+#     def init(self, hand, score):
+#         self.hand = hand
+#         self.score = score
+
+#     def take_cards(self):
